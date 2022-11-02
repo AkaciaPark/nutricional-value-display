ba15b6e44853894b43bf2283678198d17b6842ce
Akacia <akaciapark@gmail.com>
2022-11-02 14:45:36 -0300

    fixed the images and add some text

--- app/src/main/java/com/example/nutricionalvaluedisplay/MainActivity.kt
+++ app/src/main/java/com/example/nutricionalvaluedisplay/MainActivity.kt
@@ -11,8 +11,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         title = "Nutricional Value Display"
         val textView: TextView = findViewById(R.id.textView)
-        val text: String ="Hello, let's learn more about nutrients and calories on fruits!"
-        textView.text = text
         textView.movementMethod = ScrollingMovementMethod()
     }
 }
